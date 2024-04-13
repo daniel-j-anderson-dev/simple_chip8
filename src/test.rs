5d7ad234aa4cd31b65ba4fd53d7ffe704acff263
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-04-13 18:32:57 -0400

    fixed test

--- src/test.rs
+++ src/test.rs
@@ -2,11 +2,11 @@
 
 use super::*;
 
-// #[test]
-// fn create_opcode() {
-//     let most_significant = 0xF0;
-//     let least_significant = 0x0F;
-//     let opcode = merge_bytes(most_significant, least_significant);
-//     let expected_opcode: u16 = 0xF00F;
-//     assert_eq!(opcode, expected_opcode)
-// }
+#[test]
+fn create_opcode() {
+    let most_significant = 0xF0;
+    let least_significant = 0x0F;
+    let opcode = Opcode::merge_bytes(most_significant, least_significant);
+    let expected_opcode = Opcode::from(0xF00F);
+    assert_eq!(opcode, expected_opcode);
+}
