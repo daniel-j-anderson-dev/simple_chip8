003a95ad6e7ba13d61ae70ba7618814a7f26ad0b
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-04-14 00:36:27 -0400

    added doc comments

--- src/memory.rs
+++ src/memory.rs
@@ -1,3 +1,5 @@
+//! This module contains the definitions of [Memory], [AddressStack], and [Opcode]
+
 pub mod opcode;
 pub mod stack;
 
@@ -6,21 +8,34 @@ pub use self::{opcode::*, stack::*};
 use std::ops::{Deref, DerefMut};
 
 #[derive(Debug)]
+/// This struct represents the internal memory and program counter of a Chip8 interpreter
+/// - The capacity is fixed and must be known at compile time
+/// - [Memory] can be [Deref]renceded as a u8 slice so all slice methods are available
 pub struct Memory<const CAPACITY: usize> {
     bytes: [u8; CAPACITY],
     program_counter: u16,
 }
 impl<const N: usize> Memory<N> {
+    /// returns the current program counter
     pub fn get_program_counter(&self) -> u16 {
         return self.program_counter;
     }
+    /// returns a mut reference to the current program counter
     pub fn get_program_counter_mut(&mut self) -> &mut u16 {
         return &mut self.program_counter;
     }
+    /// increments the program counter by 2 (each instruction is 2 bytes)
+    /// ## Returns
+    /// - [Some] if the `program_counter` was incremented
+    /// - [None] if the `program_counter` was not modified
     pub fn increment_program_counter(&mut self) -> Option<()> {
         self.program_counter = self.program_counter.checked_add(2)?;
         return Some(());
     }
+    /// Reads the current and next byte and merges them into an opcode.
+    /// ## Returns
+    /// - [Some] if the `program_counter` AND `program_counter + 1` are in bounds
+    /// - [None] if the `program_counter` AND `program_counter + 1` are out of bounds
     pub fn get_current_instruction(&self) -> Option<Opcode> {
         let program_counter = self.program_counter as usize;
         let most_significant = self.get(program_counter)?;
@@ -32,16 +47,19 @@ impl<const N: usize> Memory<N> {
 
 impl<const N: usize> Deref for Memory<N> {
     type Target = [u8; N];
+    /// Allow all immutable slice methods
     fn deref(&self) -> &Self::Target {
         return &self.bytes;
     }
 }
 impl<const N: usize> DerefMut for Memory<N> {
+    /// Allow all mutable slice methods
     fn deref_mut(&mut self) -> &mut Self::Target {
         return &mut self.bytes;
     }
 }
 impl<const N: usize> Default for Memory<N> {
+    /// default to all zeros
     fn default() -> Self {
         Self {
             bytes: [0; N],
--- src/memory/opcode.rs
+++ src/memory/opcode.rs
@@ -1,6 +1,9 @@
+//! This module contains the definition of [Opcode]
+
 use std::ops::{Deref, DerefMut};
 
 #[derive(Debug, Default, PartialEq, Eq)]
+/// This struct is a wrapper around a [u16] value. It can easily be split up into nibbles and different arguments of an opcode.
 pub struct Opcode(u16);
 impl Opcode {
     pub fn merge_bytes(most_significant: u8, least_significant: u8) -> Opcode {
@@ -9,10 +12,11 @@ impl Opcode {
 
         return Self((most_significant << 8) | least_significant);
     }
-    /// - `self.nibbles[0]` most significant hexadecimal digit
-    /// - `self.nibbles[1]`
-    /// - `self.nibbles[2]`
-    /// - `self.nibbles[3]` least significant hexadecimal digit
+    /// Returns an array containing the opcode as an array of four nibbles.
+    /// - `nibbles[0]` most significant hexadecimal digit
+    /// - `nibbles[1]`
+    /// - `nibbles[2]`
+    /// - `nibbles[3]` least significant hexadecimal digit
     pub fn nibbles(&self) -> [u8; 4] {
         return [
             (self.0 & 0xF000) as u8,
@@ -44,18 +48,20 @@ impl Opcode {
 }
 
 impl From<u16> for Opcode {
+    /// simple constructor
     fn from(value: u16) -> Self {
         return Self(value);
     }
 }
 impl Deref for Opcode {
     type Target = u16;
-
+    /// allow [Opcode] to use all immutable [u16] methods
     fn deref(&self) -> &Self::Target {
         return &self.0;
     }
 }
 impl DerefMut for Opcode {
+    /// allow [Opcode] to use all mutable [u16] methods
     fn deref_mut(&mut self) -> &mut Self::Target {
         return &mut self.0;
     }
